52c27ecc3415ddd6ce3894289cf631bf22523f74
Hudinid <bjhu4481cobra@gmail.com>
2023-02-26 19:18:45 -0800

    basic pseudo

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 
+# 3 letter combination of o,a,A,O - make each letter unique and different
+def createCipher():
+    # probably some recursion 
+    retMonkeyCipher = {}
+
+    return retMonkeyCipher
+ 
+def decodeMonkeyCipher(currLegend, message): 
+    #probably split message by 3 and use currLegend to add characters individually to decodedMessage
+    decodedMessage = ""
+
+    return decodedMessage
+
 def main():
     print("we love monke")
     print("monke slap oooaoaoaoaoaOOAOaoAOoaaoAOO")
