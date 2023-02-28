f53ba2d51e7f66099d9c139e1126fd99fa03596e
Hudinid <bjhu4481cobra@gmail.com>
2023-02-27 18:44:57 -0800

    monke

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 
 # 3 letter combination of o,a,A,O - make each letter unique and different
+letterBank = ['a','o','A','O']
+
+def makePossibilities(usedIndices, currAns):
+    temp = usedIndices.copy()
+    continuedAns = currAns.copy()
+
+    for i in range(0, len(letterBank)):
+        print("hi")
+        
+
+
+
 def createCipher():
     # probably some recursion 
     retMonkeyCipher = {}
 
+
+
     return retMonkeyCipher
- 
+
 def decodeMonkeyCipher(currLegend, message): 
     #probably split message by 3 and use currLegend to add characters individually to decodedMessage
     decodedMessage = ""
